e00696d7d71c4e4bb436996985bb4e7037636a57
Toliver Jue <teejae@gmail.com>
2012-02-18 11:07:54 +0900

    add example file

---
+++ example/example.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "libxml"
+)
+
+func main() {
+    ParseHTML("<html><body><div id='boo'>hey</div></body></html>")
+}
+
+func ParseHTML(src string) bool {
+    d := libxml.HtmlReadDoc(src, "", "",
+        libxml.HTML_PARSE_COMPACT | libxml.HTML_PARSE_NOBLANKS |
+        libxml.HTML_PARSE_NOERROR | libxml.HTML_PARSE_NOWARNING)
+
+    defer libxml.XmlFreeDoc(d) //free doc on exit
+
+    //get root node
+    root := libxml.XmlDocGetRootElement(d);
+    if root == nil { return false } //no nodes
+
+    //traverse tree
+    var n libxml.XmlNode; n.Ptr = root
+    NextNode(&n)
+
+    return true
+}
+
+func NextNode(node *libxml.XmlNode) {
+    var curNode libxml.XmlNode
+    var childNode libxml.XmlNode
+
+    for curNode.Ptr = node.Ptr; curNode.Ptr != nil; curNode.Ptr =
+libxml.NodeNext(curNode.Ptr) {
+        //Do something here...
+        fmt.Println("NODE > ", libxml.NodeName(curNode.Ptr), " TYPE > ",
+libxml.NodeType(curNode.Ptr))
+
+        childNode.Ptr = libxml.NodeChildren(curNode.Ptr)
+        NextNode(&childNode)
+    }
+}
